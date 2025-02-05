dd9c54caeab7a7f9e7e31f6acbb8004b1eb147da
Geert-Jan Knapen <gknapen@geertjanknapen.nl>
2025-02-05 20:28:33 +0100

    Projecten pagina's en notificatie dat website WIP is

---
+++ js/main.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function() {
+    alert('Hey daar! Deze website is nog niet helemaal af, hier en daar kan het een en ander nog niet werken of je nergens naartoe brengen. Sorry voor het ongemak!');
+});
