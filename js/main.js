9c0490e79dc0c18bc5155460c6636d7ca7d5bc29
Geert-Jan Knapen <gknapen@geertjanknapen.nl>
2025-02-06 20:56:46 +0100

    Persistente theme-toggle toevoegen

--- js/main.js
+++ js/main.js
@@ -1,3 +1,56 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // alert('Hey daar! Deze website is nog niet helemaal af, hier en daar kan het een en ander nog niet werken of je nergens naartoe brengen. Sorry voor het ongemak!');
+document.addEventListener('DOMContentLoaded', function () {
+    const htmlElement = document.documentElement;
+    const navbar = document.getElementById('navbar');
+    const toggleButton = document.getElementById('theme-toggle');
+    const themeIcon = document.getElementById('theme-icon');
+
+    // See if there is a storedTheme available.
+    let storedTheme = window.localStorage.getItem('stored_theme') ?? null;
+
+    // Call toggleTheme with the storedTheme to persist the theme after navigation events.
+    toggleTheme(navbar, themeIcon, htmlElement, storedTheme);
+
+    // Add 'click' event listener that toggles the theme (don't pass a stored theme so it's overwritten.)
+    toggleButton.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        toggleTheme(navbar, themeIcon, htmlElement, null);
+    });
 });
+
+toggleTheme = (navbar, themeIcon, htmlElement, storedTheme) => {
+
+    let currentTheme, newTheme, currentColor, newColor, currentBackground, newBackground, oldIcon, newIcon;
+
+    if (storedTheme !== null) {
+        let currentTheme = storedTheme;
+        newTheme = currentTheme;
+        currentColor = (currentTheme === 'light') ? 'text-dark' : 'text-white';
+        newColor = (currentColor === 'text-white') ? 'text-dark' : 'text-white';
+        currentBackground = (currentTheme === 'light') ? 'bg-dark' : 'bg-white';
+        newBackground  = (currentBackground === 'bg-white') ? 'bg-dark' : 'bg-white';
+        oldIcon = (currentTheme === 'light') ? 'fa-sun' : 'fa-moon';
+        newIcon = (oldIcon === 'fa-moon') ? 'fa-sun' : 'fa-moon';
+    } else {
+        currentTheme = htmlElement.getAttribute('data-bs-theme');
+        newTheme = (currentTheme === 'light') ? 'dark' : 'light';
+        currentColor = (currentTheme === 'light') ? 'text-white' : 'text-dark';
+        newColor = (currentColor === 'text-white') ? 'text-dark' : 'text-white';
+        currentBackground = (currentTheme === 'light') ? 'bg-white' : 'bg-dark';
+        newBackground  = (currentBackground === 'bg-white') ? 'bg-dark' : 'bg-white';
+        oldIcon = (currentTheme === 'light') ? 'fa-moon' : 'fa-sun';
+        newIcon = (oldIcon === 'fa-moon') ? 'fa-sun' : 'fa-moon';
+    }
+
+    // Overwrite storedTheme with newTheme
+    window.localStorage.setItem('stored_theme', newTheme);
+
+    navbar.classList.remove(newBackground);
+    navbar.classList.remove(currentColor);
+    navbar.classList.add(newColor);
+    navbar.classList.add(currentBackground);
+    themeIcon.classList.remove(oldIcon);
+    themeIcon.classList.add(newIcon);
+    
+    htmlElement.setAttribute('data-bs-theme', newTheme);
+}
